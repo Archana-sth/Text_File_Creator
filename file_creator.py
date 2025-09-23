8a3560f5e82a095bca977c7bd7b45f46a549f4be
Archana Khaiyarguli <archanasth765@gmail.com>
2025-09-23 14:46:10 -0400

    Python Learning

---
+++ file_creator.py
@@ -0,0 +1,8 @@
+name = input("What's your name? \n")
+color = input("What's your favourite color? \n  ")
+
+
+
+with open('example.txt', 'w') as file:
+    file.write(f"{name} created this file")
+    file.write(f"{color} created this file")
